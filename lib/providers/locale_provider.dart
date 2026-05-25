47228c05f0f8f5cdaf94223a89200832c0cdd76e
peter <peter_krn@hotmail.com>
2026-05-25 13:25:28 +0700

    feat(providers): persist locale to secure storage and update interceptor language

--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../core/network/api_interceptor.dart';
 
 /// Provider for managing app locale (Bahasa Indonesia default)
+/// Persists locale choice to flutter_secure_storage
 class LocaleNotifier extends StateNotifier<Locale> {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
   LocaleNotifier() : super(const Locale('id'));
 
   void setLocale(String languageCode) {
     state = Locale(languageCode);
+    setInterceptorLanguage(languageCode);
+    _storage.write(key: 'app_locale', value: languageCode);
   }
 
   void toggleLocale() {
-    state = state.languageCode == 'id' ? const Locale('en') : const Locale('id');
+    final next = state.languageCode == 'id' ? 'en' : 'id';
+    setLocale(next);
   }
 }
 
